1f63349da51549754530b508133445e8147f8ee7
shamil-am <shamil.mammadov1@gmail.com>
2021-07-23 17:18:22 +0400

    next page btn done

--- src/components/OurRecentWork/RecentWorks.js
+++ src/components/OurRecentWork/RecentWorks.js
@@ -4,6 +4,7 @@ import Filterizr from "filterizr";
 import { Link } from "react-router-dom";
 import ControlCameraIcon from "@material-ui/icons/ControlCamera";
 import LinkIcon from "@material-ui/icons/Link";
+import ArrowRightAltIcon from "@material-ui/icons/ArrowRightAlt";
 
 //images
 import watch from "../../assets/images/ministor-port1.jpg";
@@ -15,21 +16,191 @@ import flower from "../../assets/images/ministor-port6.jpg";
 
 /////
 function RecentWorks() {
+  const [page, setPage] = useState(1);
+
   useEffect(() => {
-    const options = {};
-    const filterizr = new Filterizr(".filter-container", options);
-    [...document.querySelectorAll(".categoryBtn")][0].classList.add("active");
+    try {
+      const options = {};
+      const filterizr = new Filterizr(".filter-container", options);
+      // from page 2 or more return to first page reseting selected category and set 'all category' active like didmount first time
+      const allCategory = document.querySelectorAll(".categoryBtn");
+      [...allCategory].forEach((category) => {
+        category.classList.remove("active");
+      });
+      [...document.querySelectorAll(".categoryBtn")][0].classList.add("active");
+      // for fixing -- 'filterizr' is assigned a value but never used error
+      console.log(filterizr);
+      console.clear();
+    } catch (error) {
+      console.log(error);
+    }
   });
 
   const makeActive = (e) => {
     const allCategory = document.querySelectorAll(".categoryBtn");
-    console.log();
     [...allCategory].forEach((category) => {
       category.classList.remove("active");
     });
     e.target.classList.add("active");
   };
+
+  const pageChanger = (e, page) => {
+    const allPages = document.querySelectorAll(".nextPage");
+    [...allPages].forEach((pages) => {
+      pages.classList.remove("active");
+    });
+    e.target.classList.add("active");
+    setPage(page);
+  };
   //
+
+  const pageOneRender = () => {
+    return (
+      <div className="row recentWorkDisplay">
+        <div className="filter-container col-12">
+          <div
+            className="filtr-item col-12 col-md-6 col-lg-4"
+            data-category="1,3"
+            data-sort="value"
+          >
+            <img src={watch} alt="watch" />
+            <div className="imageTextHolder">
+              <h5>Demo Media Title 1</h5>
+              <p>Business,It</p>
+            </div>
+            <div className="imageLinkHolder">
+              <Link to="/">
+                <ControlCameraIcon />
+              </Link>
+              <Link to="/">
+                <LinkIcon />
+              </Link>
+            </div>
+          </div>
+          <div
+            className="filtr-item col-12 col-md-6 col-lg-4"
+            data-category="1,5,6"
+            data-sort="value"
+          >
+            <div className="imageHolder">
+              <img src={scissors} alt="scissors" />
+              <div className="imageTextHolder">
+                <h5>Demo Media Title 2</h5>
+                <p>Business,It</p>
+              </div>
+              <div className="imageLinkHolder">
+                <Link to="/">
+                  <ControlCameraIcon />
+                </Link>
+                <Link to="/">
+                  <LinkIcon />
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div
+            className="filtr-item col-12 col-md-6 col-lg-4"
+            data-category="2,5"
+            data-sort="value"
+          >
+            <div className="imageHolder">
+              <img src={coffe} alt="coffe" />
+              <div className="imageTextHolder">
+                <h5>Demo Media Title 3</h5>
+                <p>Business,It</p>
+              </div>
+              <div className="imageLinkHolder">
+                <Link to="/">
+                  <ControlCameraIcon />
+                </Link>
+                <Link to="/">
+                  <LinkIcon />
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div
+            className="filtr-item col-12 col-md-6 col-lg-4"
+            data-category="2,4"
+            data-sort="value"
+          >
+            <div className="imageHolder">
+              <img src={vase} alt="vase" />
+              <div className="imageTextHolder">
+                <h5>Demo Media Title 4</h5>
+                <p>Business,It</p>
+              </div>
+              <div className="imageLinkHolder">
+                <Link to="/">
+                  <ControlCameraIcon />
+                </Link>
+                <Link to="/">
+                  <LinkIcon />
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div
+            className="filtr-item col-12 col-md-6 col-lg-4"
+            data-category="2,5"
+            data-sort="value"
+          >
+            <div className="imageHolder">
+              <img src={flower} alt="flower" />
+              <div className="imageTextHolder">
+                <h5>Demo Media Title 5</h5>
+                <p>Business,It</p>
+              </div>
+              <div className="imageLinkHolder">
+                <Link to="/">
+                  <ControlCameraIcon />
+                </Link>
+                <Link to="/">
+                  <LinkIcon />
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div
+            className="filtr-item col-12 col-md-6 col-lg-4"
+            data-category="3,4"
+            data-sort="value"
+          >
+            <div className="imageHolder">
+              <img src={radio} alt="radio" />
+              <div className="imageTextHolder">
+                <h5>Demo Media Title 6</h5>
+                <p>Business,It</p>
+              </div>
+              <div className="imageLinkHolder">
+                <Link to="/">
+                  <ControlCameraIcon />
+                </Link>
+                <Link to="/">
+                  <LinkIcon />
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+  const pageTwoRender = () => {
+    return <h1>Page Two</h1>;
+  };
+  const allPageRender = (page) => {
+    switch (page) {
+      case 1:
+        return pageOneRender();
+      case 2:
+        return pageTwoRender();
+      case 3:
+        return <h1>Next page</h1>;
+      default:
+        <h1>404</h1>;
+    }
+  };
   return (
     <div className="recentWorks">
       <div className="container d-flex flex-column">
@@ -95,133 +266,17 @@ function RecentWorks() {
             </div>
           </div>
         </div>
-        <div className="row recentWorkDisplay">
-          <div className="filter-container col-12">
-            <div
-              className="filtr-item col-12 col-md-6 col-lg-4"
-              data-category="1,3"
-              data-sort="value"
-            >
-              <img src={watch} alt="watch" />
-              <div className="imageTextHolder">
-                <h5>Demo Media Title 1</h5>
-                <p>Business,It</p>
-              </div>
-              <div className="imageLinkHolder">
-                <Link to="/">
-                  <ControlCameraIcon />
-                </Link>
-                <Link to="/">
-                  <LinkIcon />
-                </Link>
-              </div>
-            </div>
-            <div
-              className="filtr-item col-12 col-md-6 col-lg-4"
-              data-category="1,5,6"
-              data-sort="value"
-            >
-              <div className="imageHolder">
-                <img src={scissors} alt="scissors" />
-                <div className="imageTextHolder">
-                  <h5>Demo Media Title 1</h5>
-                  <p>Business,It</p>
-                </div>
-                <div className="imageLinkHolder">
-                  <Link to="/">
-                    <ControlCameraIcon />
-                  </Link>
-                  <Link to="/">
-                    <LinkIcon />
-                  </Link>
-                </div>
-              </div>
-            </div>
-            <div
-              className="filtr-item col-12 col-md-6 col-lg-4"
-              data-category="2,5"
-              data-sort="value"
-            >
-              <div className="imageHolder">
-                <img src={coffe} alt="coffe" />
-                <div className="imageTextHolder">
-                  <h5>Demo Media Title 1</h5>
-                  <p>Business,It</p>
-                </div>
-                <div className="imageLinkHolder">
-                  <Link to="/">
-                    <ControlCameraIcon />
-                  </Link>
-                  <Link to="/">
-                    <LinkIcon />
-                  </Link>
-                </div>
-              </div>
-            </div>
-            <div
-              className="filtr-item col-12 col-md-6 col-lg-4"
-              data-category="2,4"
-              data-sort="value"
-            >
-              <div className="imageHolder">
-                <img src={vase} alt="vase" />
-                <div className="imageTextHolder">
-                  <h5>Demo Media Title 1</h5>
-                  <p>Business,It</p>
-                </div>
-                <div className="imageLinkHolder">
-                  <Link to="/">
-                    <ControlCameraIcon />
-                  </Link>
-                  <Link to="/">
-                    <LinkIcon />
-                  </Link>
-                </div>
-              </div>
-            </div>
-            <div
-              className="filtr-item col-12 col-md-6 col-lg-4"
-              data-category="2,5"
-              data-sort="value"
-            >
-              <div className="imageHolder">
-                <img src={flower} alt="flower" />
-                <div className="imageTextHolder">
-                  <h5>Demo Media Title 1</h5>
-                  <p>Business,It</p>
-                </div>
-                <div className="imageLinkHolder">
-                  <Link to="/">
-                    <ControlCameraIcon />
-                  </Link>
-                  <Link to="/">
-                    <LinkIcon />
-                  </Link>
-                </div>
-              </div>
-            </div>
-            <div
-              className="filtr-item col-12 col-md-6 col-lg-4"
-              data-category="3,4"
-              data-sort="value"
-            >
-              <div className="imageHolder">
-                <img src={radio} alt="radio" />
-                <div className="imageTextHolder">
-                  <h5>Demo Media Title 1</h5>
-                  <p>Business,It</p>
-                </div>
-                <div className="imageLinkHolder">
-                  <Link to="/">
-                    <ControlCameraIcon />
-                  </Link>
-                  <Link to="/">
-                    <LinkIcon />
-                  </Link>
-                </div>
-              </div>
-            </div>
-          </div>
+        {allPageRender(page)}
+        <div className="recentWorkPages">
+          <Link to="/" onClick={(e) => pageChanger(e, 1)} className="nextPage">
+            1
+          </Link>
+          <Link to="/" onClick={(e) => pageChanger(e, 2)} className="nextPage">
+            2
+          </Link>
+          <Link to="/" onClick={(e) => pageChanger(e, 3)} className="nextPage">
+            <ArrowRightAltIcon />
+          </Link>
         </div>
       </div>
     </div>
